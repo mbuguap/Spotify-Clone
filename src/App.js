7d2a047a8ded53a4d9919f7fafbc2dc65733d762
mbuguap <petrmbugua@gmail.com>
2022-10-05 17:41:26 +0300

    vcvb

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
+import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
+ 
 
 function App() {
   return (
